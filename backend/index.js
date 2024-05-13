ae960a626bac072fc7492536c388e9a137282d74
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2024-05-13 09:16:09 +0200

    Integrated moralis

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,74 @@
 require('dotenv').config();
+
 const express = require("express");
+const Moralis = require("moralis").default;
+const { EvmChain } = require("@moralisweb3/common-evm-utils");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
+const address = "0x0Ef8296c36b7AcfD82F0b8d35A70bf43E8D23bd4";
+const chain = EvmChain.ETHEREUM;
+
+async function getDemoData() {
+  // Get native balance
+  const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
+    address,
+    chain,
+  });
+
+  // Format the native balance formatted in ether via the .ether getter
+  const native = nativeBalance.result.balance.ether;
+
+  // Get token balances
+  const tokenBalances = await Moralis.EvmApi.token.getWalletTokenBalances({
+    address,
+    chain,
+  });
+
+  // Format the balances to a readable output with the .display() method
+  const tokens = tokenBalances.result.map((token) => token.display());
+
+  // Get the nfts
+  const nftsBalances = await Moralis.EvmApi.nft.getWalletNFTs({
+    address,
+    chain,
+    limit: 10,
+  });
+
+  // Format the output to return name, amount and metadata
+  const nfts = nftsBalances.result.map((nft) => ({
+    name: nft.result.name,
+    amount: nft.result.amount,
+    metadata: nft.result.metadata,
+  }));
+
+  return { native, tokens, nfts };
+}
+
+app.get("/demo", async (req, res) => {
+  try {
+    // Get and return the crypto data
+    const data = await getDemoData();
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500);
+    res.json({ error: error.message });
+  }
 });
 
-app.listen(port, () => {
-    console.log(`App is listening to port ${port}`);
-});
+const startServer = async () => {
+  await Moralis.start({
+    apiKey: MORALIS_API_KEY,
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+startServer();
