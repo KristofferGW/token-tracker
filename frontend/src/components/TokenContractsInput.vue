80ea2ff06d1db4daacc425262d0b1c8fdac6bc04
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2024-05-16 15:42:42 +0200

    unstyled frontend

---
+++ frontend/src/components/TokenContractsInput.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      tokenContracts: ''
+    };
+  }
+};
+</script>
+
+<template>
+    <div>
+      <label for="tokenContracts">Token Contract Addresses:</label>
+      <textarea id="tokenContracts" v-model="tokenContracts" rows="4" cols="50"></textarea>
+    </div>
+  </template>
